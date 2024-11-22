fb66ba273286f60f9155067136eb5855e25d1347
nabil <abboud@outlook.com>
2024-11-22 10:51:24 +0100

    exo 2

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -3,22 +3,36 @@
 Fixed::Fixed()
 {
     std::cout << "Default constructor called" << std::endl;
-    _fix = 0;
+    _value = 0;
+}
+
+Fixed::Fixed(const int intValue)
+{
+    std::cout << "Int constructor called\n";
+    _value = intValue << _bits;
+
+}
+
+Fixed::Fixed(const float floatValue)
+{
+    std::cout << "Float constructor called\n";
+    _value = roundf(floatValue * (1 << _bits));
+
 }
 
 Fixed::Fixed(const Fixed& src)
 {
     std::cout << "Copy constructor called" << std::endl;
-    _fix = src.getRawBits();
+    _value = src.getRawBits();
 }
 
-Fixed& Fixed::operator=(Fixed& other)
+Fixed& Fixed::operator=(const Fixed& other)
 {
 
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
     {
-        _fix = other.getRawBits();
+        _value = other.getRawBits();
     }
     return *this;
 }
@@ -31,12 +45,27 @@ Fixed::~Fixed()
 int Fixed::getRawBits() const
 {
     std::cout << "getRawBits member function called" << std::endl;
-    return _fix;
+    return _value;
 }
 
 void Fixed::setRawBits(int i)
 {
-    _fix = i;
+    _value = i;
+}
+
+float Fixed::toFloat(void) const 
+{
+    return static_cast<float>(_value) / (1 << _bits);
+}
+
+int Fixed::toInt(void) const 
+{
+    return _value >> _bits;
+}
+
+std::ostream& operator<<(std::ostream &out, const Fixed &fixed) {
+    out << fixed.toFloat();  
+    return out;
 }
 
 
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -3,21 +3,29 @@
 
 #include <string>
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
     public: 
         Fixed();
+        Fixed(const int intValue);
+        Fixed(const float floatValue);
         Fixed(const Fixed& src);
-        Fixed& operator=(Fixed& other);
+        Fixed& operator=(const Fixed& other);
         ~Fixed();
+
 
         int getRawBits() const;
         void setRawBits(int i);
+        int toInt(void) const;
+        float toFloat(void) const;
+        
+        friend std::ostream& operator<<(std::ostream &out, const Fixed &fixed);
 
     private:
-        int _fix;
-        const static int _bits;
+        int _value;
+        const static int _bits = 8;
 
 };
 
